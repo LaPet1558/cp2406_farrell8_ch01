e5f916651ac668852c677bcf04f5731fbaacd4d5
Deborah Chew <debs1558@gmail.com>
2018-03-17 14:59:36 +0800

    Fix debugging exercises

---
+++ DebuggingExercises/FixDebugOne1.java
@@ -0,0 +1,8 @@
+public class FixDebugOne1 {
+
+    /* This program displays a greeting */
+    public static void main(String[] args) {
+        /* Systemoutprintl("Hello). */
+        System.out.println("Hello.");
+    }
+}
